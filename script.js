f50e8d09c1770ab7b4650ac2733200d912c55b47
Santhosh <spinisetti6@gmail.com>
2023-10-30 12:18:42 +0530

    commit Pointer Game

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-const rect = document.querySelector("#center");
-rect.addEventListener("mousemove", function(dets){
+window.addEventListener("mousemove", function(dets){
+    var rect = document.querySelector("#center");
     // console.log(dets.clientX);
+    var rectVal = gsap.utils.mapRange(0, window.innerWidth, 200+rect.getBoundingClientRect().width/2, window.innerWidth-(200+rect.getBoundingClientRect().width/2),dets.clientX);
     gsap.to("#center",{
-        left:dets.clientX + "px",
+        // left:dets.clientX + "px",
+        left:rectVal,
         ease:Power3,
     });
-})
+});
